84debe6cb79377531d614cf566c41451790fe993
juyeon_Prestige16 <zooy2on01@gmail.com>
2023-12-06 06:17:05 +0900

    게임 배경 구현
    운동장 위치 수정 필요

---
+++ 2DGP_PROJECT_1VS1SOCCER/code/game_background.py
@@ -0,0 +1,23 @@
+from pico2d import *
+import game_world
+
+Screen_x, Screen_y = 1920, 1080
+
+
+# [game_background] 게임 배경 레이어
+class GameBackGround:
+    def __init__(self):
+        self.background = load_image('GAME_ROUND/BackGround.png')
+        self.ground = load_image('GAME_ROUND/Ground.png')
+        self.x, self.y = Screen_x // 2, Screen_y // 2
+        self.ground_x = Screen_x // 2
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.background.clip_draw(0, 0, Screen_x, Screen_y, self.x, self.y)
+        self.ground.clip_draw(0, 0, 6000, 460, self.ground_x, 600, 2000, 460)
+
+    def handle_events(self, e):
+        pass
