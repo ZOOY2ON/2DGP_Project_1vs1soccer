f2edadcc2d297ef68b583067ec181bceb9397e62
juyeon_Prestige16 <zooy2on01@gmail.com>
2023-12-06 10:59:12 +0900

    공의 x축 이동 구현

--- 2DGP_PROJECT_1VS1SOCCER/code/game_character.py
+++ 2DGP_PROJECT_1VS1SOCCER/code/game_character.py
@@ -1,31 +1,76 @@
 from pico2d import *
+import math
 from game_start import GameStart
 
 
 Screen_x, Screen_y = 1920, 1080
 
+class Ball:
+    def __init__(self, x, y, radius, speed, is_moving):
+        self.x = x               # 공의 x 좌표
+        self.y = y               # 공의 y 좌표
+        self.radius = radius     # 공의 반지름
+        self.speed = speed       # 공의 이동 속도
+        self.is_moving = is_moving  # 공의 움직임 여부
+
+        self.dx = 0              # 공의 x 방향 속도
+        self.dy = 0              # 공의 y 방향 속도
+
+        # === 공
+        self.ball = load_image('GAME_ROUND/Ball.png')
+
+    def update(self):
+        if self.is_moving:
+            self.x += self.dx * self.speed
+            self.y += self.dy * self.speed
+
+    def draw(self):
+        self.ball.clip_draw(0, 0, 41, 41, self.x, self.y, 60, 60)
+
+    def set_movement(self, dx, dy):
+        self.dx = dx
+        self.dy = dy
 
 # [game_character] 게임 캐릭터 구현
 class GameCharacter:
     def __init__(self):
-        self.ch_01_st = {'speed': 10, 'power': 3, 'jump': 14}
-        self.ch_02_st = {'speed': 6, 'power': 4, 'jump': 11}
 
         # === 캐릭터
         self.character_01 = load_image('GAME_CHARACTER/CharacterSheet.png')
         self.character_02 = load_image('GAME_CHARACTER/CharacterSheet.png')
+        self.character_01_flip = load_image('GAME_CHARACTER/CharacterSheetFlip.png')
+        self.character_02_flip = load_image('GAME_CHARACTER/CharacterSheetFlip.png')
 
-        # === 설정
-        self.x, self.y = Screen_x // 2, Screen_y // 2
-        self.ch_x_01, self.ch_x_02 = Screen_x * 0.3, Screen_x * 0.7     # 캐릭터 초기 위치
-        self.ch_y_01, self.ch_y_02 = 300, 300  # 캐릭터 초기 위치
+        # === 캐릭터 설정
+        self.ch_01_st = {'speed': 10, 'power': 3, 'jump': 13}   #5/3/3
+        self.ch_02_st = {'speed': 6, 'power': 5, 'jump': 13}    #3/5/3
 
-        self.bottom_01, self.bottom_02 = 0,1
-        self.frame_01, self.frame_02 = 0, 0
+        self.ch_x_01, self.ch_x_02 = Screen_x * 0.3, Screen_x * 0.7     # 캐릭터 초기 위치 x 좌표
+        self.ch_y_01, self.ch_y_02 = 300, 300  # 캐릭터 초기 위치 y 좌표
+
         self.dir_01, self.dir_02 = 0, 0     # 1 : right, -1 : left
         self.jump_01, self.jump_02 = False, False
-        self.jump_01_count, self.jump_02_count = 0, 0       # ???
+        self.jump_01_count, self.jump_02_count = 0, 0
         self.jump_speed_01, self.jump_speed_02 = 2, 2
+
+        self.character_1, self.character_2 = 1, 0   # 좌우대칭 확인용
+        self.ch_r = 174 // 2
+
+        # === 공
+        self.ball = load_image('GAME_ROUND/Ball.png')
+
+        # === 공 설정
+        #self.ball_x, self.ball_y = Screen_x // 2, 230
+        #self.ball_r = 60 // 2
+        #self.ball_dx, self.ball_dy = 0, 0
+
+        # === 공 객체 생성 (정지 상태)
+        self.static_ball = Ball(Screen_x // 2, 230, 30, 3, False)   # x / y / 반지름 / 속도 / 움직임 여부
+
+        # === 기본 설정
+        self.x, self.y = Screen_x // 2, Screen_y // 2
+        self.bottom_01, self.bottom_02 = 0, 4
+        self.frame_01, self.frame_02 = 0, 0
 
         print("GameCharacter")
 
@@ -33,6 +78,83 @@ class GameCharacter:
         self.frame_01 = (self.frame_01 + 1) % 3
         self.frame_02 = (self.frame_02 + 1) % 3
 
+        self.update_character()
+        self.check_collision()
+
+    def check_collision(self):
+        # 공과 캐릭터 1의 충돌 검사
+        distance_01 = math.sqrt((self.ch_x_01 - self.static_ball.x)**2 + (self.ch_y_01 - self.static_ball.y)**2)
+        if distance_01 < self.ch_r + self.static_ball.radius:
+            # 충돌 시 튕김
+            self.bounce_ball(self.ch_01_st['power'], self.ch_x_01, self.ch_y_01)
+            print("충돌 발생 : ball-01")
+
+        # 공과 캐릭터 2의 충돌 검사
+        distance_02 = math.sqrt((self.ch_x_02 - self.static_ball.x)**2 + (self.ch_y_02 - self.static_ball.y)**2)
+        if distance_02 < self.ch_r + self.static_ball.radius:
+            # 충돌 시 튕김
+            self.bounce_ball(self.ch_02_st['power'], self.ch_x_02, self.ch_y_02)
+            print("충돌 발생 : ball-02")
+
+    def bounce_ball(self, power, character_x, character_y):
+        # 충돌 시 튕김 로직
+        direction_x = self.static_ball.x - character_x
+        direction_y = self.static_ball.y - character_y
+        distance = math.sqrt(direction_x ** 2 + direction_y ** 2)
+
+        if distance != 0:  # 0으로 나누는 경우 방지
+            direction_x /= distance
+            direction_y /= distance
+
+            # 튕겨나가는 거리 계산
+            bounce_distance = power * 5
+
+            # 공의 위치 업데이트
+            self.static_ball.x = character_x + direction_x * (self.ch_r + self.static_ball.radius + bounce_distance)
+            #self.static_ball.y = character_y + direction_y * (self.ch_r + self.static_ball.radius + bounce_distance)
+
+            # 공의 이동 방향 업데이트
+            self.static_ball.dx = direction_x
+            #self.static_ball.dy = direction_y
+
+    def draw(self):
+        self.draw_character()
+        #self.ball.clip_draw(0, 0, 41, 41, self.ball_x,self.ball_y, 60, 60)
+        # === 정지 상태의 공 그리기
+        self.static_ball.draw()
+
+    def handle_events(self, e):
+        self.move_character(e)
+
+    # === 캐릭터
+    def draw_character(self):
+        # 캐릭터 1 그리기
+        if self.dir_01 == 1:  # 우측 바라보기
+            self.character_01_flip.clip_draw(self.frame_01 * 174, self.bottom_01 * 174, 174, 174, self.ch_x_01, self.ch_y_01)
+            self.character_1 = 1
+        elif self.dir_01 == -1:  # 좌측 바라보기 (좌우 대칭)
+            self.character_01.clip_draw(self.frame_01 * 174, self.bottom_01 * 174, 174, 174, self.ch_x_01, self.ch_y_01)
+            self.character_1 = 0
+        else:
+            if self.character_1 == 1:
+                self.character_01_flip.clip_draw(174, self.bottom_01 * 174, 174, 174, self.ch_x_01, self.ch_y_01)
+            else:
+                self.character_01.clip_draw(174, self.bottom_01 * 174, 174, 174, self.ch_x_01,self.ch_y_01)
+
+        # 캐릭터 2 그리기
+        if self.dir_02 == 1:  # 우측 바라보기
+            self.character_02_flip.clip_draw(self.frame_02 * 174, self.bottom_02 * 174, 174, 174, self.ch_x_02, self.ch_y_02)
+            self.character_2 = 1
+        elif self.dir_02 == -1:  # 좌측 바라보기 (좌우 대칭)
+            self.character_02.clip_draw(self.frame_02 * 174, self.bottom_02 * 174, 174, 174, self.ch_x_02, self.ch_y_02)
+            self.character_2 = 0
+        else:
+            if self.character_2 == 1:
+                self.character_02_flip.clip_draw(174, self.bottom_02 * 174, 174, 174, self.ch_x_02, self.ch_y_02)
+            else:
+                self.character_02.clip_draw(174, self.bottom_02 * 174, 174, 174, self.ch_x_02, self.ch_y_02)
+
+    def update_character(self):
         # 캐릭터 1 이동 및 점프 로직
         if self.dir_01 == 1:  # 우측 이동 중
             self.ch_x_01 += self.ch_01_st['speed']
@@ -65,24 +187,7 @@ class GameCharacter:
                 self.jump_speed_02 = 0
                 self.jump_02 = False
 
-    def draw(self):
-        # 캐릭터 1 그리기
-        if self.dir_01 == 1:  # 우측 바라보기
-            self.character_01.clip_draw(self.frame_01 * 174, self.bottom_01 * 174, 174, 174, self.ch_x_01, self.ch_y_01)
-        elif self.dir_01 == -1:  # 좌측 바라보기 (좌우 대칭)
-            self.character_01.clip_draw(self.frame_01 * 174, self.bottom_01 * 174, 174, 174, self.ch_x_01, self.ch_y_01)
-        else:
-            self.character_01.clip_draw(174, self.bottom_01 * 174, 174, 174, self.ch_x_01, self.ch_y_01)
-
-        # 캐릭터 2 그리기
-        if self.dir_02 == 1:  # 우측 바라보기
-            self.character_02.clip_draw(self.frame_02 * 174, self.bottom_02 * 174, 174, 174, self.ch_x_02, self.ch_y_02)
-        elif self.dir_02 == -1:  # 좌측 바라보기 (좌우 대칭)
-            self.character_02.clip_draw(self.frame_02 * 174, self.bottom_02 * 174, 174, 174, self.ch_x_02, self.ch_y_02)
-        else:
-            self.character_02.clip_draw(174, self.bottom_02 * 174, 174, 174, self.ch_x_02, self.ch_y_02)
-
-    def handle_events(self, e):
+    def move_character(self,e):
         if e.type == SDL_KEYDOWN:
             # === 캐릭터 1 움직임 동작
             if e.key == SDLK_w and not self.jump_01:
